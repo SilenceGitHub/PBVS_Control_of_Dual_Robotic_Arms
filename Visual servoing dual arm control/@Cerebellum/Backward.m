8b4aa6281324c80e68b15c84655ee3a8cf9f0a7e
SilenceGitHub <109529313+SilenceGitHub@users.noreply.github.com>
2022-09-19 18:56:54 +0800

    Delete Visual servoing dual arm control directory

--- Visual servoing dual arm control/@Cerebellum/Backward.m
+++
@@ -1,6 +0,0 @@
-function ret = Backward(obj,error)
-    % update output weight of ESN
-    deltaWout = obj.learningRate * newTanh(error) * obj.cell';
-    obj.Wout = obj.Wout + deltaWout;
-    ret = obj;
-end
--- Visual servoing dual arm control/@Kuka/SetJointAnglePosition.m
+++
@@ -1,7 +0,0 @@
-function [] = SetJointAnglePosition(obj,vrepInstance,input, opmode)
-
-    for i = 1:obj.jointNumber
-        vrepInstance.vrep.simxSetJointTargetPosition(vrepInstance.clientID, obj.joint_handle(i), input(i), opmode);
-    end
-    
-end
