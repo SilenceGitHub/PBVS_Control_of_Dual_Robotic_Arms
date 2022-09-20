a2fee8ce929f7ae21c52e530b0bbbcfb4558ff7b
SilenceGitHub <109529313+SilenceGitHub@users.noreply.github.com>
2022-09-20 13:01:55 +0800

    Update StopSim.m

--- Visual servo control for dual robotic arms/StopSim.m
+++ Visual servo control for dual robotic arms/StopSim.m
@@ -26,18 +26,27 @@ set(gca, 'ytick', yMin:0.1:yMax);
 set(gca,'FontSize',25);
 xlabel('$t$ (s)','interpreter','latex','fontsize',30); ylabel('Position error (m)','fontname','times new roman','fontsize',30);
 
+
+orien_d = zeros(4,stage2);
+orien_a = zeros(4,stage2);
+orien_d = angle2quat(ur.desiredPath(1,:), ur.desiredPath(2,:), ur.desiredPath(3,:),'XYZ')';
+orien_a = angle2quat(ur.actualPath(1,:), ur.actualPath(2,:), ur.actualPath(3,:),'XYZ')';
+orienError = orien_d - orien_a;
+
 figure
-xMin = 0; xMax = stage2*param.samplingGap; yMin = -0.3; yMax = 0.1;
-p4 = plot(Te(1:interval:stage2),ur.errors(4,1:interval:stage2)','-', 'linewidth', 3); hold on;
-p5 = plot(Te(1:interval:stage2),ur.errors(5,1:interval:stage2)',':', 'linewidth', 3);
-p6 = plot(Te(1:interval:stage2),ur.errors(6,1:interval:stage2)','--', 'linewidth', 3);
-plot([stage1*param.samplingGap,stage1*param.samplingGap],[yMin,yMax],'g--','linewidth',3);hold off;
-legend('$e_4$','$e_5$','$e_6$','fontsize',30,'interpreter','latex');
+xMin = 0; xMax = stage2*param.samplingGap; yMin = -0.05; yMax = 0.1;
+p4 = plot(Te(1:interval:stage2),orienError(1,1:interval:stage2)','-', 'linewidth', 4); hold on;
+p5 = plot(Te(1:interval:stage2),orienError(2,1:interval:stage2)',':', 'linewidth', 4);
+p6 = plot(Te(1:interval:stage2),orienError(3,1:interval:stage2)','--', 'linewidth', 4);
+p6 = plot(Te(1:interval:stage2),orienError(4,1:interval:stage2)','-.', 'linewidth', 4);
+plot([stage1*param.samplingGap,stage1*param.samplingGap],[yMin,yMax],'g--','linewidth',4);hold off;
+legend('$e_4$','$e_5$','$e_6$','$e_7$','fontsize',32,'interpreter','latex','NumColumns',4);
 axis([xMin xMax yMin yMax]);
 set(gca, 'xtick', xMin:4:xMax);
-set(gca, 'ytick', yMin:0.1:yMax);
-set(gca,'FontSize',25);
-xlabel('$t$ (s)','interpreter','latex','fontsize',30); ylabel('Orientation error (rad)','fontname','times new roman','fontsize',30);
+set(gca, 'ytick', yMin:0.05:yMax);
+set(gca,'FontSize',28);
+xlabel('$t$ (s)','interpreter','latex','fontsize',32); ylabel('Orientation error','fontname','times new roman','fontsize',32);
+
 
 
 figure
